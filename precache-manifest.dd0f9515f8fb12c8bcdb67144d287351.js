ffff3f8902647bc19c2c61e936d89eca3d8b2647
Anushree <anushreerdesai@gmail.com>
2020-09-21 02:53:24 -0400

    build folder

---
+++ precache-manifest.dd0f9515f8fb12c8bcdb67144d287351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833dd5b81a247b8d31704d578edd2779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ee394c629c23e8cd8e",
+    "url": "/static/css/main.9fce4fe3.chunk.css"
+  },
+  {
+    "revision": "ff79c393f90c6f2b45a5",
+    "url": "/static/js/2.4d585fce.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.4d585fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee394c629c23e8cd8e",
+    "url": "/static/js/main.19058a7e.chunk.js"
+  },
+  {
+    "revision": "c5e33494d1804b0aff6d",
+    "url": "/static/js/runtime-main.b325f3ba.js"
+  }
+]);
